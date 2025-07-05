b0bb4250fdcc5c953e452be3653cfde84388bf49
isahaq1 <hmisahaq01@gmail.com>
2025-07-05 15:25:17 +0600

    first commit

--- src/BarcodeGenerator.php
+++ src/BarcodeGenerator.php
@@ -23,11 +23,36 @@ class BarcodeGenerator
         ];
 
         $this->barcodeTypes = [
-            'CODE128' => [104, 'Code 128'],
-            'CODE39' => [39, 'Code 39'],
-            'EAN13' => [13, 'EAN-13'],
+            'C39' => [39, 'Code 39'],
+            'C39+' => [39, 'Code 39+'],
+            'C39E' => [39, 'Code 39 Extended'],
+            'C39E+' => [39, 'Code 39 Extended+'],
+            'C93' => [93, 'Code 93'],
+            'S25' => [25, 'Standard 2 of 5'],
+            'S25+' => [25, 'Standard 2 of 5+'],
+            'I25' => [25, 'Interleaved 2 of 5'],
+            'I25+' => [25, 'Interleaved 2 of 5+'],
+            'C128' => [128, 'Code 128'],
+            'C128A' => [128, 'Code 128 A'],
+            'C128B' => [128, 'Code 128 B'],
+            'C128C' => [128, 'Code 128 C'],
+            'EAN2' => [2, 'EAN 2'],
+            'EAN5' => [5, 'EAN 5'],
+            'EAN8' => [8, 'EAN 8'],
+            'EAN13' => [13, 'EAN 13'],
             'UPCA' => [12, 'UPC-A'],
-            'EAN8' => [8, 'EAN-8']
+            'UPCE' => [12, 'UPC-E'],
+            'MSI' => [0, 'MSI'],
+            'MSI+' => [0, 'MSI+'],
+            'POSTNET' => [0, 'POSTNET'],
+            'PLANET' => [0, 'PLANET'],
+            'RMS4CC' => [0, 'RMS4CC'],
+            'KIX' => [0, 'KIX'],
+            'IMB' => [0, 'IMB'],
+            'CODABAR' => [0, 'Codabar'],
+            'CODE11' => [11, 'Code 11'],
+            'PHARMA' => [0, 'Pharma Code'],
+            'PHARMA2T' => [0, 'Pharma Code Two-Track']
         ];
     }
 
@@ -58,110 +83,180 @@ class BarcodeGenerator
 
         $bars = [];
         switch ($type) {
-            case 'CODE128':
-                $bars = $this->encodeCode128($code);
+            case 'C128':
+            case 'C128A':
+            case 'C128B':
+            case 'C128C':
+                $bars = $this->encodeCode128($code, $type);
                 break;
-            case 'CODE39':
-                // Implement basic Code 39 encoding
-                $validChars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-. $/+%';
-                $code = strtoupper($code);
-
-                // Validate input
-                if (!preg_match('/^[' . preg_quote($validChars, '/') . ']+$/', $code)) {
-                    throw new Exception('Invalid characters in Code 39 barcode');
-                }
-
-                $bars = [];
-                $patterns = [
-                    '0' => [1, 0, 1, 1, 0, 1, 0, 0], '1' => [1, 1, 0, 1, 0, 0, 1, 0],
-                    '2' => [1, 0, 0, 1, 0, 0, 1, 0], '3' => [1, 1, 0, 0, 1, 0, 0, 0],
-                    '4' => [1, 0, 1, 1, 0, 0, 1, 0], '5' => [1, 1, 0, 1, 1, 0, 0, 0],
-                    '6' => [1, 0, 0, 1, 1, 0, 0, 0], '7' => [1, 0, 1, 0, 0, 1, 0, 0],
-                    '8' => [1, 1, 0, 0, 0, 1, 0, 0], '9' => [1, 0, 0, 0, 1, 0, 0, 0],
-                    'A' => [1, 1, 0, 1, 0, 1, 0, 0], 'B' => [1, 0, 0, 1, 0, 1, 0, 0],
-                    'C' => [1, 1, 0, 0, 1, 1, 0, 0], 'D' => [1, 0, 1, 1, 0, 1, 0, 0],
-                    'E' => [1, 1, 0, 1, 1, 1, 0, 0], 'F' => [1, 0, 0, 1, 1, 1, 0, 0],
-                    '-' => [1, 0, 1, 0, 1, 1, 0, 0], '.' => [1, 1, 0, 0, 1, 0, 1, 0],
-                    ' ' => [1, 0, 0, 1, 0, 1, 1, 0], '$' => [1, 0, 1, 0, 1, 0, 1, 0],
-                    '/' => [1, 0, 1, 0, 0, 1, 1, 0], '+' => [1, 0, 1, 1, 0, 0, 0, 0],
-                    '%' => [1, 1, 0, 0, 0, 0, 1, 0]
-                ];
-
-                // Add start character *
-                $bars = array_merge($bars, [1, 0, 1, 0, 0, 1, 1, 0]);
-
-                // Encode each character
-                foreach (str_split($code) as $char) {
-                    $bars = array_merge($bars, $patterns[$char]);
-                }
-
-                // Add stop character *
-                $bars = array_merge($bars, [1, 0, 1, 0, 0, 1, 1, 0]);
-
-                return $bars;
+            case 'C39':
+            case 'C39+':
+            case 'C39E':
+            case 'C39E+':
+                $bars = $this->encodeCode39($code, $type);
                 break;
-                // Add other barcode type implementations here
+            case 'C93':
+                $bars = $this->encodeCode93($code);
+                break;
+            case 'S25':
+            case 'S25+':
+                $bars = $this->encodeStandard2of5($code, $type);
+                break;
+            case 'I25':
+            case 'I25+':
+                $bars = $this->encodeInterleaved2of5($code, $type);
+                break;
+            case 'EAN13':
+            case 'EAN8':
+            case 'EAN5':
+            case 'EAN2':
+                $bars = $this->encodeEAN($code, $type);
+                break;
+            case 'UPCA':
+            case 'UPCE':
+                $bars = $this->encodeUPC($code, $type);
+                break;
+            case 'MSI':
+            case 'MSI+':
+                $bars = $this->encodeMSI($code, $type);
+                break;
+            case 'CODABAR':
+                $bars = $this->encodeCodabar($code);
+                break;
+            case 'CODE11':
+                $bars = $this->encodeCode11($code);
+                break;
+            case 'PHARMA':
+            case 'PHARMA2T':
+                $bars = $this->encodePharma($code, $type);
+                break;
+            case 'POSTNET':
+            case 'PLANET':
+            case 'RMS4CC':
+            case 'KIX':
+            case 'IMB':
+                $bars = $this->encodePostal($code, $type);
+                break;
+            default:
+                throw new Exception("Encoding not implemented for type: $type");
         }
 
         return $bars;
     }
 
-    protected function createBarcodeImage(array $bars, array $options): \GdImage
+    // Add implementation methods for each barcode type
+    protected function encodeCode128(string $code, string $type): array
     {
-        $width = count($bars) * $options['width'];
-        $height = $options['height'];
+        // Implement Code 128 encoding with support for A, B, and C variants
+        // Return array of bars
+    }
 
-        $image = imagecreatetruecolor($width + ($options['padding'] * 2), $height + ($options['padding'] * 2));
+    protected function encodeCode39(string $code, string $type): array
+    {
+        // Implement Code 39 encoding with support for extended and checksum variants
+        // Return array of bars
+    }
 
-        // Set background color
-        $bgColor = imagecolorallocate($image, ...$options['background_color']);
-        $fgColor = imagecolorallocate($image, ...$options['foreground_color']);
+    // Add other encoding methods...
+}
+{
+    // Implement basic Code 39 encoding
+    $validChars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-. $/+%';
+    $code = strtoupper($code);
 
-        imagefill($image, 0, 0, $bgColor);
+    // Validate input
+    if (!preg_match('/^[' . preg_quote($validChars, '/') . ']+$/', $code)) {
+        throw new Exception('Invalid characters in Code 39 barcode');
+    }
 
-        // Draw bars
-        $x = $options['padding'];
-        foreach ($bars as $bar) {
-            if ($bar) {
-                imagefilledrectangle(
-                    $image,
-                    $x,
-                    $options['padding'],
-                    $x + $options['width'] - 1,
-                    $options['padding'] + $height - 1,
-                    $fgColor
-                );
-            }
-            $x += $options['width'];
+    $bars = [];
+    $patterns = [
+        '0' => [1, 0, 1, 1, 0, 1, 0, 0], '1' => [1, 1, 0, 1, 0, 0, 1, 0],
+        '2' => [1, 0, 0, 1, 0, 0, 1, 0], '3' => [1, 1, 0, 0, 1, 0, 0, 0],
+        '4' => [1, 0, 1, 1, 0, 0, 1, 0], '5' => [1, 1, 0, 1, 1, 0, 0, 0],
+        '6' => [1, 0, 0, 1, 1, 0, 0, 0], '7' => [1, 0, 1, 0, 0, 1, 0, 0],
+        '8' => [1, 1, 0, 0, 0, 1, 0, 0], '9' => [1, 0, 0, 0, 1, 0, 0, 0],
+        'A' => [1, 1, 0, 1, 0, 1, 0, 0], 'B' => [1, 0, 0, 1, 0, 1, 0, 0],
+        'C' => [1, 1, 0, 0, 1, 1, 0, 0], 'D' => [1, 0, 1, 1, 0, 1, 0, 0],
+        'E' => [1, 1, 0, 1, 1, 1, 0, 0], 'F' => [1, 0, 0, 1, 1, 1, 0, 0],
+        '-' => [1, 0, 1, 0, 1, 1, 0, 0], '.' => [1, 1, 0, 0, 1, 0, 1, 0],
+        ' ' => [1, 0, 0, 1, 0, 1, 1, 0], '$' => [1, 0, 1, 0, 1, 0, 1, 0],
+        '/' => [1, 0, 1, 0, 0, 1, 1, 0], '+' => [1, 0, 1, 1, 0, 0, 0, 0],
+        '%' => [1, 1, 0, 0, 0, 0, 1, 0]
+    ];
+
+    // Add start character *
+    $bars = array_merge($bars, [1, 0, 1, 0, 0, 1, 1, 0]);
+
+    // Encode each character
+    foreach (str_split($code) as $char) {
+        $bars = array_merge($bars, $patterns[$char]);
+    }
+
+    // Add stop character *
+    $bars = array_merge($bars, [1, 0, 1, 0, 0, 1, 1, 0]);
+
+    return $bars;
+    break;
+    // Add other barcode type implementations here
+}
+
+protected function createBarcodeImage(array $bars, array $options): \GdImage
+{
+    $width = count($bars) * $options['width'];
+    $height = $options['height'];
+
+    $image = imagecreatetruecolor($width + ($options['padding'] * 2), $height + ($options['padding'] * 2));
+
+    // Set background color
+    $bgColor = imagecolorallocate($image, ...$options['background_color']);
+    $fgColor = imagecolorallocate($image, ...$options['foreground_color']);
+
+    imagefill($image, 0, 0, $bgColor);
+
+    // Draw bars
+    $x = $options['padding'];
+    foreach ($bars as $bar) {
+        if ($bar) {
+            imagefilledrectangle(
+                $image,
+                $x,
+                $options['padding'],
+                $x + $options['width'] - 1,
+                $options['padding'] + $height - 1,
+                $fgColor
+            );
         }
-
-        return $image;
+        $x += $options['width'];
     }
 
-    protected function encodeCode128(string $code): array
-    {
-        // Implement Code 128 encoding logic here
-        // This is a simplified version, you'll need to implement the full spec
-        $bars = [];
-        foreach (str_split($code) as $char) {
-            $charCode = ord($char);
-            // Add the bar pattern for each character
-            $bars = array_merge($bars, $this->getCode128Pattern($charCode));
-        }
-        return $bars;
-    }
+    return $image;
+}
 
-    protected function getCode128Pattern(int $charCode): array
-    {
-        // Define the bar patterns for Code 128
-        // This is a simplified version, you'll need to implement the full spec
-        $patterns = [
-            // Define patterns for each character
-            // 1 represents a bar, 0 represents a space
-            32 => [1, 1, 0, 1, 1, 0, 0],  // space
-            // Add more patterns here
-        ];
-
-        return $patterns[$charCode] ?? array_fill(0, 7, 0);
+protected function encodeCode128(string $code): array
+{
+    // Implement Code 128 encoding logic here
+    // This is a simplified version, you'll need to implement the full spec
+    $bars = [];
+    foreach (str_split($code) as $char) {
+        $charCode = ord($char);
+        // Add the bar pattern for each character
+        $bars = array_merge($bars, $this->getCode128Pattern($charCode));
     }
+    return $bars;
+}
+
+protected function getCode128Pattern(int $charCode): array
+{
+    // Define the bar patterns for Code 128
+    // This is a simplified version, you'll need to implement the full spec
+    $patterns = [
+        // Define patterns for each character
+        // 1 represents a bar, 0 represents a space
+        32 => [1, 1, 0, 1, 1, 0, 0],  // space
+        // Add more patterns here
+    ];
+
+    return $patterns[$charCode] ?? array_fill(0, 7, 0);
+}
 }
